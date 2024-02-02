55e503167fa24532adcdf4360eacaf3e947eef35
beposso006 <belenposso2006@gmail.com>
2024-02-02 14:43:14 -0600

    primer commit

---
+++ Lab3P2_LinsyPosso/src/lab3p2_linsyposso/Pokemon.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3p2_linsyposso;
+
+/**
+ *
+ * @author 29164
+ */
+public class Pokemon {
+    private String nombre;
+    private int numPokedex;
+    private String naturaleza;
+    private boolean atrapado;
+    Pokeball pokebola;
+
+    public Pokemon(String nombre, int numPokedex, String naturaleza, boolean atrapado, Pokeball pokebola) {
+        this.nombre = nombre;
+        this.numPokedex = numPokedex;
+        this.naturaleza = naturaleza;
+        this.atrapado = false;
+        this.pokebola = null;
+    }
+    
+    
+}
---
+++ Lab3P2_LinsyPosso/src/lab3p2_linsyposso/WaterType.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3p2_linsyposso;
+
+/**
+ *
+ * @author 29164
+ */
+public class WaterType extends Pokemon {
+    private boolean vidaEnAgua;
+    private int rapidezNado;
+}
